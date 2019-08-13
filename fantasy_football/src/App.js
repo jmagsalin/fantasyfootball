cfde959ff2a1999bdffad310bbeaf1f2ebd45a5b
jmagsalin <jmagsalin@hotmail.com>
2019-08-13 16:43:29 -0700

    added pages

--- fantasy_football/src/App.js
+++ fantasy_football/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import BoxScore from './boxscore/boxscore'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
           Hello World!!
+          A
         </p>
         <a
           className="App-link"
@@ -18,7 +19,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+        
         </a>
+  
       </header>
     </div>
   );
--- fantasy_football/src/index.js
+++ fantasy_football/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Boxscore from './boxscore/boxscore';
+import BoxscorePlayer from './boxscore-player/boxscore-player';
+import Client from './client/client';
+import FreeAgentPlayer from './free-agent-player/free-agent-player';
+import Player from './player/player';
+import PlayerStats from './player-stats/player-stats';
+import Team from './team/team';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +17,13 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+export {
+    Boxscore,
+    BoxscorePlayer,
+    Client,
+    FreeAgentPlayer,
+    Player,
+    PlayerStats,
+    Team
+  };
